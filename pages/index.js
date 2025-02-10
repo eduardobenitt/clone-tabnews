b325334861f5a5e8c59d1a4daf9de1862e49e46a
Eduardo Benitt <benitt.eduardo@gmail.com>
2025-02-10 02:30:28 +0000

    Alteração de Mensagem e Adição de imagem do Chico

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import React from "react";
+import chicoImage from './images/chico.jpg'
+
 function Home(){
-    return <h1>Eu amo você, Maria Eduarda Pacheco Anunciato. Também amo muito o Chiquin fedido. ❤</h1>
+    return  (
+        <><div>
+            <h1>Eu amo você, Maria Eduarda Pacheco Anunciato. Também amo muito o Chiquin fedido. ❤</h1>
+        </div>
+        <div>
+            <img src={chicoImage} alt="Chiquin" />
+        </div></>        
+
+    );
 }
 
 export default Home;
