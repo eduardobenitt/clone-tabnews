3cb3c48a4d3e070fa09bf9ce7e9ae60eb5c1eeb1
Eduardo Benitt <benitt.eduardo@gmail.com>
2025-02-10 02:39:37 +0000

    Ajuste na imagem

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,13 @@ function Home(){
             <h1>Eu amo você, Maria Eduarda Pacheco Anunciato. Também amo muito o Chiquin fedido. ❤</h1>
         </div>
         <div>
-            <img src="/images/chico.jpg" alt="Chiquin" />
-        </div></>        
+            <Image
+                src="/images/chico.jpg"
+                alt="Chiquin"
+                width={500}
+                height={300}
+            />    
+         </div></>        
 
     );
 }
