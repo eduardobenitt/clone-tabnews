7a92daf0f844de5647e0af74a5619c0c6447a444
Eduardo Benitt <benitt.eduardo@gmail.com>
2025-02-10 03:12:44 +0000

    Alteração da imagem

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Image from "next/image";
 
-function Home(){
-    return  (
-        <><div>
-            <h1>Eu amo você, Maria Eduarda Pacheco Anunciato. Também amo muito o Chiquin fedido. ❤</h1>
-        </div>
-        <div>
-            <Image
-                src="/images/chico.PNG"
-                alt="Chiquin"
-                width={500}
-                height={300}
-            />    
-         </div></>        
-
-    );
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>
+          Eu amo você, Maria Eduarda Pacheco Anunciato. Também amo muito o
+          Chiquin fedido. ❤
+        </h1>
+      </div>
+      <div>
+        <Image
+          src="/images/chico.png"
+          alt="Chiquin"
+          width={500}
+          height={300}
+        />
+      </div>
+    </>
+  );
 }
 
 export default Home;
